11bc9eb4bfbd03618f421995dd971164c27fa2da
Alekh <just4mme@gmail.com>
2018-01-15 18:56:16 +0530

    Setting up start and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 app.set('view engine', 'hbs'); //specify the type of file to be used
 
@@ -49,6 +50,6 @@ app.get('/bad', (req, res) => {
     error: 'error in loading page'
   })
 });
-app.listen(3000, () => {
-  console.log("Server up on port 3000");
+app.listen(port, () => {
+  console.log(`Server up on port ${port}`);
 });
