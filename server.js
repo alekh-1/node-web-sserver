bb733883487c143b6906a66d3333a88575d31f26
Alekh <just4mme@gmail.com>
2018-01-15 19:37:39 +0530

    Adding project page

--- server.js
+++ server.js
@@ -8,29 +8,31 @@ app.set('view engine', 'hbs'); //specify the type of file to be used
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('upperCase', (text) => text.toUpperCase());
+
+
 app.use((req, res, next) => {
   var now = new Date().toString();
   console.log(`${now} : ${req.method} ${req.url}`);
   next();
-})
+});
 
 // app.use((req, res, next) => {
 //   res.render('site.hbs')
 // })
 
-app.get('/', (req, res) => {
-  res.send({
-    name: 'Alekh',
-    title: {
-      inside: "hello"
-    }
-  });
-});
+
+
 
 // app.get('/about', (req, res) => {
 //   res.send("About Page");
 // });
 
+app.get('/', (req, res) => {
+  res.send("Hello World");
+
+});
 app.get('/about', (req, res) => {
   res.render('aboutPage.hbs', {
     pageTitle: 'About Page',
@@ -41,15 +43,24 @@ app.get('/about', (req, res) => {
 app.get('/welcome', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Welcome Page',
-    message: "Chant Hare Krishna",
-    currentYear: new Date().getFullYear()
+    message: "This is the welcome page",
+  
   })
-})
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Github Projects',
+    message: 'You can see my Github projects here',
+  })
+});
+
 app.get('/bad', (req, res) => {
   res.send( {
     error: 'error in loading page'
   })
 });
+
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
